a8f0fe4634bd0b589ccc9b233b8d44da05717340
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-02-27 12:54:45 +0100

    update character
    moved Enum outside the character class.

--- Models/Domain/Character.cs
+++ Models/Domain/Character.cs
@@ -2,13 +2,14 @@
 
 namespace MovieApi.Models.Domain
 {
+    public enum Genders { Male, Female, Other }
+
     [Table("Character")]
     public class Character
     {
         public int Id { get; set; }
         public string ? FullName { get; set; }
         public string ? Alias { get; set; }
-        public enum Genders { Male,Female,Other} 
         public Genders ? Gender { get; set; }
         public string ? PictureUrl { get; set; }
 
