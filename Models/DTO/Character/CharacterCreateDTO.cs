f20ba81cf4f0d6101a4fb14d204761997a6449a7
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-02-28 09:31:53 +0100

    Updated Character Service, CharacterController
    added CRUD to character service and controller

--- Models/DTO/Character/CharacterCreateDTO.cs
+++ Models/DTO/Character/CharacterCreateDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace MovieApi.Models.DTO.Character
+﻿using MovieApi.Models.Domain;
+
+namespace MovieApi.Models.DTO.Character
 {
     public class CharacterCreateDTO
     {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Alias { get; set; }
+        public Genders? Gender { get; set; }
+        public string? PictureUrl { get; set; }
+
+        public ICollection<Domain.Movie>? AppearInMovies { get; set; }
     }
 }
