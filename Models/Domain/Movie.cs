731afa6bc9d7fa949c1a19e1ae9d33e7994aed16
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-02-28 14:23:08 +0100

    Changed Genre field to string type. Added character collection relationship.

--- Models/Domain/Movie.cs
+++ Models/Domain/Movie.cs
@@ -1,23 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieApi.Models.Domain
 {
     [Table("Movie")]
     public class Movie
     {
+        // PK
         public int Id { get; set; }
+        // Fields
+        [MaxLength(50)]
         public string ? MovieTitle { get; set; }
-        public enum Genres { }
-        public Genres ? Genre { get; set; }
-
+        [MaxLength(50)]
+        public string ? Genre { get; set; }
         public DateTime ? ReleaseYear { get; set; }
-
+        [MaxLength(50)]
         public string ? Director { get; set; }
-
+        [MaxLength(100)]
         public string ? PictureUrl { get; set; }
-
+        [MaxLength(100)]
         public string ? TrailerUrl { get; set; }
-
-        public Franchise ? MovieFranchise { get; set; }
+        // Relationships
+        public Franchise ? Franchise { get; set; }
+        public ICollection<Character> ? Characters { get; set; }
     }
 }
