dc393d3391ab2cc516b9a974fcf2ee8fd2b6f039
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-03-01 15:50:42 +0100

    Added FranchiseController with Service. Added ICrudService interface.

--- Controllers/Franchises/FranchiseController.cs
+++ Controllers/Franchises/FranchiseController.cs
@@ -1,8 +1,173 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Models;
+using MovieApi.Models.Domain;
+using MovieApi.Models.DTO.Franchises;
+using MovieApi.Models.DTO.Movies;
+using MovieApi.Services.Franchises;
 
 namespace MovieApi.Controllers.Franchises
 {
+    [Route("api/v1/[controller]")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+
+    [ApiController]
     public class FranchiseController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly IFranchiseService _franchiseService;
+        //private readonly MovieDbContext _context;
+
+        public FranchiseController(IMapper mapper, IFranchiseService franchiseService)
+        {
+            _mapper = mapper;
+            _franchiseService = franchiseService;
+            //_context = context;
+        }
+
+        // GET: api/Franchise
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FranchiseReadDTO>>> GetFranchises()
+        {
+            return Ok(_mapper.Map<List<FranchiseReadDTO>>(await _franchiseService.GetAllAsync()));
+        }
+
+        // GET: api/Franchise/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FranchiseReadDTO>> GetFranchise(int id)
+        {
+            try
+            {
+                return Ok(_mapper.Map<FranchiseReadDTO>(
+                        await _franchiseService.GetByIdAsync(id))
+                    );
+            }
+            catch (Exception ex)
+            {
+                return NotFound(
+                    new ProblemDetails()
+                    {
+                        Detail = ex.Message,
+                        Status = ((int)HttpStatusCode.NotFound)
+                    }
+                    );
+            }
+        }
+
+        // PUT: api/Franchise/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFranchise(int id, FranchiseEditDTO franchise)
+        {
+            if (id != franchise.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _franchiseService.UpdateAsync(
+                        _mapper.Map<Franchise>(franchise)
+                    );
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(
+                    new ProblemDetails()
+                    {
+                        Detail = ex.Message,
+                        Status = ((int)HttpStatusCode.NotFound)
+                    }
+                    );
+            }
+
+
+            return NoContent();
+        }
+
+        // POST: api/Franchise
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult> PostFranchise(FranchiseCreateDTO franchiseDto)
+        {
+            Franchise franchise = _mapper.Map<Franchise>(franchiseDto);
+            await _franchiseService.AddAsync(franchise);
+            return CreatedAtAction("GetFranchise", new { id = franchise.Id }, franchise);
+        }
+
+        // DELETE: api/Franchise/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFranchise(int id)
+        {
+            try
+            {
+                await _franchiseService.DeleteByIdAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(
+                    new ProblemDetails()
+                    {
+                        Detail = ex.Message,
+                        Status = ((int)HttpStatusCode.NotFound)
+                    }
+                    );
+            }
+        }
+
+        [HttpGet("{id}/movies")]
+        public async Task<ActionResult<IEnumerable<MovieReadDTO>>> GetMoviesForFranchise(int id)
+        {
+            try
+            {
+                return Ok(
+                        _mapper.Map<List<MovieReadDTO>>(
+                            await _franchiseService.GetMoviesAsync(id)
+                        )
+                    );
+            }
+            catch (Exception ex)
+            {
+                return NotFound(
+                    new ProblemDetails()
+                    {
+                        Detail = ex.Message,
+                        Status = ((int)HttpStatusCode.NotFound)
+                    }
+                    );
+            }
+        }
+
+        [HttpPut("{id}/movies")]
+        public async Task<IActionResult> UpdateMoviesForFranchise(int[] movieIds, int id)
+        {
+            try
+            {
+                await _franchiseService.UpdateMoviesAsync(movieIds, id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(
+                    new ProblemDetails()
+                    {
+                        Detail = ex.Message,
+                        Status = ((int)HttpStatusCode.NotFound)
+                    }
+                    );
+            }
+        }
     }
 }
