fef7950b46baedbd7dbde037f889b8ca05cbcda3
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-03-01 11:24:36 +0100

    Updated characterController, added profiles
    Updated charactercontroller to enable CRUD.
    
    added profiles. for automap

--- Controllers/Characters/CharacterController.cs
+++ Controllers/Characters/CharacterController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using MovieApi.Models.DTO.Characters;
 using System.Net.Mime;
 using AutoMapper;
+using MovieApi.Models.Domain;
 
 namespace MovieApi.Controllers.Characters
 {
@@ -31,9 +32,82 @@ namespace MovieApi.Controllers.Characters
         public async Task<ActionResult<IEnumerable<CharacterReadDTO>>> GetCharacters()
         {
             return Ok(await _context!.Character.ToListAsync());
-            //return _mapper!.Map<List<CharacterReadDTO>>(await _context!.Character
-            //    .Include(c => c.Movies)
-            //    .ToListAsync());
         }
+
+        /// <summary>
+        /// Gets a specific character by their Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CharacterReadDTO>> GetCharacterById(int id)
+        {
+            Character? character = await _context!.Character.FindAsync(id);
+
+            if (character == null) { return NotFound(); }
+
+            return Ok(character);
+        }
+
+        /// <summary>
+        /// Adds a new character to the database.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<CharacterCreateDTO>> CreateCharacter(Character character)
+        {
+            _context?.Character.Add(character);
+            await _context!.SaveChangesAsync();
+
+            return CreatedAtAction("GetCharacter",new {id = character.Id},character);
+        }
+
+
+        /// <summary>
+        /// Updates a character.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCharacter(int id, CharacterEditDTO character)
+        {
+            if (id != character.Id) {  return BadRequest(); }
+
+            Character ? domainCharacter = _mapper!.Map<Character>(character);
+            _context!.Entry(domainCharacter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CharacterExists(id)) return NotFound();
+                else { throw; }
+            }
+
+           return NoContent();
+        }
+
+        /// <summary>
+        /// Deletes a character.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCharacter(int id)
+        {
+            Character ? character = await _context!.Character.FindAsync(id);
+            if (character == null) { return NotFound(); }
+
+            _context.Character.Remove(character);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        protected bool CharacterExists(int id) => _context!.Character.Any(x => x.Id == id);
     }
 }
--- Models/DTO/Characters/CharacterEditDTO.cs
+++ Models/DTO/Characters/CharacterEditDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace MovieApi.Models.DTO.Characters
+﻿using MovieApi.Models.Domain;
+
+namespace MovieApi.Models.DTO.Characters
 {
     public class CharacterEditDTO
     {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Alias { get; set; }
+        public string? Gender { get; set; }
+        public string? PictureUrl { get; set; }
+        public ICollection<Movie>? Movies { get; set; }
     }
 }
